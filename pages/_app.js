8d9bba60c4aa8c098d0a393a1bb890048352cf62
You Song Hou <yousonghou@gmail.com>
2021-11-09 19:24:35 -0500

    create a new Next.js app

---
+++ pages/_app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from '../components/layout/Header'
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <div>
+      <Header />
+      <Component {...pageProps} />
+    </div>
+  )
+}
+
+export default MyApp;
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Collect NFT Bingo</h1>
+    </div>
+  )
+}
