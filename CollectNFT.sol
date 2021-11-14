4ac62e0212c6c6c333b081d615b056e1669e169e
You Song Hou <yousonghou@gmail.com>
2021-11-13 19:14:15 -0500

    add Chainlink VRF on the contract

--- CollectNFT.sol
+++ CollectNFT.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.12;
 
-contract CollectNFT {
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
+
+contract CollectNFT is VRFConsumerBase {
     uint public poolCount = 0;
     mapping(uint => Pool) public pools;
     
@@ -9,6 +11,29 @@ contract CollectNFT {
     mapping(uint => Image) public images;
     
     mapping(address => mapping(uint => UserCard)) public userCardList;
+    
+    bytes32 internal keyHash;
+    uint256 internal fee;
+    
+    uint256 public randomResult;        // Make this private on production
+    
+    /**
+     * Constructor inherits VRFConsumerBase
+     * 
+     * Network: Polygon (Matic) Mumbai Testnet
+     * Chainlink VRF Coordinator address: 0x8C7382F9D8f56b33781fE506E897a4F1e2d17255
+     * LINK token address:                0x326C977E6efc84E512bB9C30f76E30c160eD06FB
+     * Key Hash: 0x6e75b569a01ef56d18cab6a8e71e6600d6ce853834d4a5748b720d06f878b3a4
+     */
+    constructor() 
+        VRFConsumerBase(
+            0x8C7382F9D8f56b33781fE506E897a4F1e2d17255, // VRF Coordinator
+            0x326C977E6efc84E512bB9C30f76E30c160eD06FB  // LINK Token
+        ) public
+    {
+        keyHash = 0x6e75b569a01ef56d18cab6a8e71e6600d6ce853834d4a5748b720d06f878b3a4;
+        fee = 0.0001 * 10 ** 18; // 0.0001 LINK
+    }
     
     struct Pool {
         uint id;
@@ -43,6 +68,12 @@ contract CollectNFT {
         address payable owner
     );
     
+     event TestR (
+        bytes32 imageId,
+        uint poolId,
+        address payable owner
+    );
+    
     function createPool() external {
         poolCount++;
         pools[poolCount] = Pool(poolCount, new uint[](0), 0, msg.sender);
@@ -65,9 +96,17 @@ contract CollectNFT {
         userCardList[msg.sender][_poolId] = UserCard(_poolId, 0, new uint[](0));
     }
     
-    function earnNFTofImage() external {
-        uint _randomNumber = getRandomValue(imageCount);
-        Image storage _images = images[_randomNumber + 1];
+    function buyLootBox() external {
+        getRandomNumber();
+        
+        for(uint i = 0; i < 5; i++){
+            uint256 _randomNumber = uint256(keccak256(abi.encode(randomResult, i))) % imageCount + 1;
+            earnNFTofImage(_randomNumber);
+        }
+    }
+    
+    function earnNFTofImage(uint256 _randomNumber) internal {
+        Image storage _images = images[_randomNumber];
         
         UserCard storage _userCard = userCardList[msg.sender][_images.poolId];
         _userCard.imageIdList.push(_images.id);
@@ -121,8 +160,33 @@ contract CollectNFT {
         return true;
     }
     
-    // Return a random number 0 - 100
+    /** 
+     * Return a random number 0 - 100
+     */
     function getRandomValue(uint mod) internal view returns(uint) {
         return uint(keccak256(abi.encodePacked(now, block.difficulty, msg.sender))) % mod;
+    }
+    
+    /** 
+     * Requests randomness 
+     */
+    function getRandomNumber() public returns (bytes32 requestId) {
+        require(LINK.balanceOf(address(this)) > fee, "Not enough LINK - fill contract with faucet");
+        return requestRandomness(keyHash, fee);
+    }
+
+    /**
+     * Callback function used by VRF Coordinator
+     */
+    function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
+        randomResult = randomness;
+    }
+    
+    /**
+     * DO NOT ADD THIS IN PRODUCTION
+     * Avoid locking your LINK in the contract
+     */
+    function withdrawLink() external {
+        require(LINK.transfer(msg.sender, LINK.balanceOf(address(this))), "Unable to transfer");
     }
 }
