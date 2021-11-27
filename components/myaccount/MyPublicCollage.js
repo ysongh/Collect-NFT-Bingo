b8fbb3135f09cda8afd2ca3b7c29a7e68f48906d
You Song Hou <yousonghou@gmail.com>
2021-11-26 22:40:51 -0500

    add an Ant Design Spinner on My Public Collage page

--- components/myaccount/MyPublicCollage.js
+++ components/myaccount/MyPublicCollage.js
@@ -1,20 +1,24 @@
 import React, { useEffect, useState } from "react";
-import { Row, Col, Card } from 'antd';
+import { Row, Col, Card, Spin } from 'antd';
 
 function MyPublicCollage({ walletAddress }) {
   const [userNFTs, setUserNFTs] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const loadMyCollection = async () => {
       try{
+        setLoading(true);
         const nft = await fetch(`https://api.covalenthq.com/v1/137/address/${walletAddress}/balances_v2/?nft=true&key=${process.env.NEXT_PUBLIC_COVALENT_APIKEY}`);
         const { data } = await nft.json();
   
         console.log(data);
         console.log(data.items[0].nft_data);
         setUserNFTs(data.items[0].nft_data);
+        setLoading(false);
       } catch(error) {
         console.error(error);
+        setLoading(false);
       }
     }
     
@@ -23,12 +27,16 @@ function MyPublicCollage({ walletAddress }) {
 
   return (
     <Row gutter={[10, 10]} style={{ marginTop: '1rem' }}>
-      {userNFTs.map(nft => (
-        <Col key={nft.token_id} className="gutter-row" sm={{ span: 24 }} md={{ span: 12 }} lg={{ span: 6 }}>
-          <Card cover={<img src={nft.external_data.image} alt="Collection NFT" />}>
-          </Card>
-        </Col>
-      ))}
+      {loading
+        ? <Spin size="large" style={{ margin: '5rem auto'}}/>
+        : userNFTs.map(nft => (
+          <Col key={nft.token_id} className="gutter-row" sm={{ span: 24 }} md={{ span: 12 }} lg={{ span: 6 }}>
+            <Card cover={<img src={nft.external_data.image} alt="Collection NFT" />}>
+            </Card>
+          </Col>
+        ))
+      }
+      {!walletAddress && <p>Connect to your ETH wallet to see collections</p>}
     </Row>
   )
 }
